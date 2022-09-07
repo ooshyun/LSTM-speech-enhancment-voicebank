7e62cd4506a416d4a925d6965150f22b899aa1e0
Seunghyun Oh <seunghyun.daniel.oh@gmail.com>
2022-09-07 18:09:53 +0900

    add test using lstm

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from distutils.command.clean import clean
+from statistics import mode
 import tensorflow as tf
 
 device_name = tf.test.gpu_device_name()
@@ -7,6 +7,7 @@ if device_name != '/device:GPU:0':
 print('Found GPU at: {}'.format(device_name))
 
 import os
+from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
@@ -26,39 +27,44 @@ device_lib.list_local_devices()
 tf.random.set_seed(999)
 np.random.seed(999)
 
-model_name = 'cnn'
-# model_name = 'lstm'
+# model_name = 'cnn'
+model_name = 'lstm'
 
 path_to_dataset = f"./records_{model_name}"
 
-model = build_model(l2_strength=0.0)
-# model = build_model_lstm()
+
+if model_name == "cnn":
+  windowLength = 256
+  overlap      = round(0.25 * windowLength) # overlap of 75%
+  ffTLength    = windowLength
+  inputFs      = 48e3
+  fs           = 16e3
+  numFeatures  = ffTLength//2 + 1
+  numSegments  = 8
+  model = build_model(l2_strength=0.0)
+
+if model_name == "lstm":
+  windowLength = 512
+  overlap      = round(0.5 * windowLength) # overlap of 75%
+  ffTLength    = windowLength
+  inputFs      = 48e3
+  fs           = 16e3
+  numFeatures  = ffTLength//2 + 1
+  numSegments  = 63 # 1 sec in 512 window, 256 hop, sr = 16000 Hz
+  model = build_model_lstm()
+
 model.summary()
+model_path = Path(f'./history/{model_name}')
 
-windowLength = 256
-overlap      = round(0.25 * windowLength) # overlap of 75%
-ffTLength    = windowLength
-inputFs      = 48e3
-fs           = 16e3
-numFeatures  = ffTLength//2 + 1
-numSegments  = 8
-print("windowLength:",windowLength)
-print("overlap:",overlap)
-print("ffTLength:",ffTLength)
-print("inputFs:",inputFs)
-print("fs:",fs)
-print("numFeatures:",numFeatures)
-print("numSegments:",numSegments)
-
-# [TODO] Model load
-model = keras.models.load_model(f"./model_{model_name}")
+# Model load
+model = keras.models.load_model(model_path / "model")
 
 # [TODO] Load test wav file 
 voiceBankDEMAND_basepath = '/Users/seunghyunoh/workplace/study/NoiseReduction/Tiny-SpeechEnhancement/data/VoiceBankDEMAND/DS_10283_2791'
 voiceBank = VoiceBandDEMAND(voiceBankDEMAND_basepath, val_dataset_percent=0.3)
 clean_test_filenames, noisy_test_filenames = voiceBank.get_test_filenames()
 
-clean_file = clean_test_filenames[0]
+clean_file = clean_test_filenames[0] # select file
 noisy_file = noisy_test_filenames[0]
 
 print("Clean: ", clean_file)
@@ -79,22 +85,38 @@ noise_stft_features = noiseAudioFeatureExtractor.get_stft_spectrogram()
 # At reconstruction, noisy spectral phase was used instead to
 # perform in- verse STFT and recover human speech.
 noisyPhase = np.angle(noise_stft_features)
-print(noisyPhase.shape)
 noise_stft_features = np.abs(noise_stft_features)
 
 mean = np.mean(noise_stft_features)
 std = np.std(noise_stft_features)
 noise_stft_features = (noise_stft_features - mean) / std
 
-predictors = prepare_input_features(noise_stft_features, numSegments, numFeatures)
+if model_name == "cnn":
+  predictors = prepare_input_features(noise_stft_features, numSegments, numFeatures)
 
-predictors = np.reshape(predictors, (predictors.shape[0], predictors.shape[1], 1, predictors.shape[2]))
-predictors = np.transpose(predictors, (3, 0, 1, 2)).astype(np.float32)
-print('predictors.shape:', predictors.shape)
+  predictors = np.reshape(predictors, (predictors.shape[0], predictors.shape[1], 1, predictors.shape[2]))
+  predictors = np.transpose(predictors, (3, 0, 1, 2)).astype(np.float32)
+  print('predictors.shape:', predictors.shape)
 
-STFTFullyConvolutional = model.predict(predictors)
-print(STFTFullyConvolutional.shape)
+  STFTFullyConvolutional = model.predict(predictors)
+  print(STFTFullyConvolutional.shape)
 
+elif model_name == "lstm":
+  loss_sec = 1/fs * overlap * (noise_stft_features.shape[1] - (noise_stft_features.shape[1]//numSegments * numSegments))
+  print(f"loss...{loss_sec} sec")
+
+  noise_stft_features = noise_stft_features[:, :noise_stft_features.shape[1]//numSegments * numSegments]
+  noisyPhase = noisyPhase[:, :noise_stft_features.shape[1]//numSegments * numSegments]
+
+  predictors = np.reshape(noise_stft_features, (noise_stft_features.shape[0], noise_stft_features.shape[1]//numSegments, numSegments))
+  predictors = np.transpose(predictors, (1, 2, 0))
+  predictors = np.expand_dims(predictors, axis=1)
+  STFTFullyConvolutional = model.predict(predictors)
+  STFTFullyConvolutional = np.transpose(STFTFullyConvolutional, [1, 0, 2, 3])
+  nchannel, nsource, nsegment, fft_channels = STFTFullyConvolutional.shape
+  STFTFullyConvolutional = np.reshape(STFTFullyConvolutional, [nchannel, nsource*nsegment, fft_channels])
+  STFTFullyConvolutional = np.squeeze(STFTFullyConvolutional)
+  
 def revert_features_to_audio(features, phase, cleanMean=None, cleanStd=None):
     # scale the outpus back to the original range
     if cleanMean and cleanStd:
@@ -121,9 +143,12 @@ def revert_features_to_audio(features, phase, cleanMean=None, cleanStd=None):
 # 8 = siren
 # 9 = street_music
 
+# segment, feature, 1, 1
 denoisedAudioFullyConvolutional = revert_features_to_audio(STFTFullyConvolutional, noisyPhase, mean, std)
 print("Min:", np.min(denoisedAudioFullyConvolutional),"Max:",np.max(denoisedAudioFullyConvolutional))
 # ipd.Audio(data=denoisedAudioFullyConvolutional, rate=fs) # load a local WAV file
+
+print(denoisedAudioFullyConvolutional.shape)
 
 f, (ax1, ax2, ax3) = plt.subplots(3, 1, sharey=True)
 
